2c216341ee5658a5e9d2f25dbed45d5505d9ef2f
Paula Roche <paularoche@Mac-mini-de-Paula.local>
2024-01-11 17:34:59 +0100

    conectado ciente a pedidos

--- interfaces/pedidos.py
+++ interfaces/pedidos.py
@@ -15,18 +15,23 @@ class   WindowOrders(Frame):
             print(order.client)
 
     def create_widgets_orders(self):
-        self.F1 = ["f1", "f2", "f3", "f4", "f5"]
-        self.B1 = ["b1", "b2", "b3", "b4", "b5"]
-        self.frame_ext_1 = Frame(self, bg='#D7DEE0', height=80)
-        self.frame_ext_1.pack(expand=True, fill='x')
-        for i in range (5):
-            self.F1[i] = Frame(self.frame_ext_1)
-            self.F1[i].pack(side='left', expand=True, fill='both')
-            self.F1[i].config(bg='#D7DEE0', bd=5, relief='ridge', padx=10, pady=6)
-            self.B1[i] = Button(self.F1[i], text="hola", command=self.go_to_order)
-            self.B1[i].grid(row=0, column=0)
-            self.F1[i].grid_rowconfigure(0, weight=1)
-            self.B1[i].config(font=("Verdana", 14), bg='#D7DEE0', padx=2, pady=2)
+        self.F = []
+        self.B = []
+        for i in range(len(self.all_orders.list_unpaid_orders)):
+            f = "f" + str(i)
+            b = "b" + str(i)
+            self.F.append(f)
+            self.B.append(b)
+        self.orders_frame = Frame(self, bg='#D7DEE0', height=300)
+        self.orders_frame.pack(expand=True, fill='x')
+        for i in range (len(self.F)):
+            self.F[i] = Frame(self.orders_frame)
+            self.F[i].pack(side='left', expand=True, fill='both')
+            self.F[i].config(bg='#D7DEE0', bd=5, relief='ridge', padx=10, pady=6)
+            self.B[i] = Button(self.F[i], text=self.all_orders.list_unpaid_orders[i].client, command=self.go_to_order)
+            self.B[i].grid(row=0, column=0)
+            self.F[i].grid_rowconfigure(0, weight=1)
+            self.B[i].config(font=("Verdana", 14), bg='#D7DEE0', padx=2, pady=2)
         # n_frames = len(self.all_orders.list_unpaid_orders)
         # self.orders_frame = Frame(self, bg='#E8E5E6', height=80)
         # self.pack(expand=True, fill='x')
@@ -37,7 +42,7 @@ class   WindowOrders(Frame):
 
     def destroy_widgets_orders(self):
         self.orders_frame.destroy()
-        self.button_prueba.destroy()
+        #self.button_prueba.destroy()
 
     def go_to_order(self):
         pass
